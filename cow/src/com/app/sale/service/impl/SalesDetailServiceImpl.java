7c0b05f70d2fce8158c80618228af8b6d20da15f
kruthika <harshithashanthraju@gmail.com>
2026-05-28 22:16:16 +0530

    validate dto & service

--- cow/src/com/app/sale/service/impl/SalesDetailServiceImpl.java
+++ cow/src/com/app/sale/service/impl/SalesDetailServiceImpl.java
@@ -8,6 +8,14 @@ public class SalesDetailServiceImpl  implements SalesDetailService {
     @Override
     public boolean validateAndSave(SalesDetailDTO detailDTO) {
         System.out.println("data preserved:"+detailDTO);
+        String SallerName= detailDTO.getSallername();
+        if(SallerName!=null && SallerName.length()>=3 && SallerName.length()<=80 && SallerName.matches("[a-zA-Z .]+"))
+        {
+            System.out.println("valid name");
+        }
+        else{
+            System.out.println("invalid");
+        }
         return false;
     }
 }
