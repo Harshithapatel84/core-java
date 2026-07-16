16f175aebb045702825a4dbcecdc5ba543989b1e
kruthika <harshithashanthraju@gmail.com>
2026-07-16 21:32:02 +0530

    set implentation

--- countryFilter/src/com/xworkz/country/runner/SetRunner.java
+++ countryFilter/src/com/xworkz/country/runner/SetRunner.java
@@ -18,5 +18,15 @@ public class SetRunner {
         System.out.println("total size is:"+runner.size());
         runner.forEach(r-> System.out.println(r));
 
+        Set<Number> numbers=new HashSet<>();
+        numbers.add(56);
+        numbers.add(67);
+        numbers.add(68);
+        numbers.add(96);
+        numbers.add(34);
+        numbers.add(67);
+        
+        numbers.forEach(System.out::println);
+
     }
 }
