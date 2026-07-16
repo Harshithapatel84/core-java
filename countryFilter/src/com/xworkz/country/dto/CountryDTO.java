a343655ca9825fe9a99d0313a90325bb0f21696f
kruthika <harshithashanthraju@gmail.com>
2026-07-16 21:28:39 +0530

    set implentation

---
+++ countryFilter/src/com/xworkz/country/dto/CountryDTO.java
@@ -0,0 +1,10 @@
+package com.xworkz.country.dto;
+
+import java.io.Serializable;
+
+public class CountryDTO implements Serializable {
+    private  String name;
+    private Integer pincode;
+    private String president;
+    private String Prime_Minister;
+}
