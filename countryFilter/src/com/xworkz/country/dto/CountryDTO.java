2f1fa8c88091dd7a6cf546e5dc11d6ed74ff3ca6
kruthika <harshithashanthraju@gmail.com>
2026-07-17 20:01:26 +0530

    set implentation

--- countryFilter/src/com/xworkz/country/dto/CountryDTO.java
+++ countryFilter/src/com/xworkz/country/dto/CountryDTO.java
@@ -1,10 +1,47 @@
 package com.xworkz.country.dto;
 
-import java.io.Serializable;
+import lombok.*;
 
-public class CountryDTO implements Serializable {
+import java.io.Serializable;
+import java.util.Objects;
+
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Getter
+
+public class CountryDTO implements Serializable,Comparable<CountryDTO>{
     private  String name;
     private Integer pincode;
     private String president;
     private String Prime_Minister;
+
+    @Override
+    public boolean equals(Object o) {
+        System.out.println("running equals");
+    return true;
+    }
+
+    @Override
+
+    public int hashCode() {
+        System.out.println("running hashCode");
+
+        int result = 17;
+        result = 31 * result + name.hashCode();
+        result = 31 * result + pincode;
+        result = 31 * result + president.hashCode();
+        result = 31 * result + Prime_Minister.hashCode();
+
+        return result;
+    }
+
+
+    @Override
+    public int compareTo(CountryDTO o) {
+        return this.name.compareTo(o.name);
+    }
 }
+
--- countryFilter/src/com/xworkz/country/runner/SetRunner.java
+++ countryFilter/src/com/xworkz/country/runner/SetRunner.java
@@ -1,7 +1,10 @@
 package com.xworkz.country.runner;
 
+import com.xworkz.country.dto.CountryDTO;
+
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SetRunner {
 
@@ -25,8 +28,19 @@ public class SetRunner {
         numbers.add(96);
         numbers.add(34);
         numbers.add(67);
-        
+
         numbers.forEach(System.out::println);
 
+        Set<CountryDTO> countryDTOS=new TreeSet<>();
+        countryDTOS.add(new CountryDTO("india",91,"draupadi murmu","narendra modi"));
+        countryDTOS.add(new CountryDTO("United States", 1, "Donald Trump", "Donald Trump"));
+        countryDTOS.add(new CountryDTO("United Kingdom", 44, "King Charles III", "Keir Starmer"));
+        countryDTOS.add(new CountryDTO("Japan", 81, "Naruhito", "Shigeru Ishiba"));
+        countryDTOS.add(new CountryDTO("Australia", 61, "Sam Mostyn", "Anthony Albanese"));
+        System.out.println("total size:"+countryDTOS.size());
+        countryDTOS.forEach(System.out::println);
+        boolean present= countryDTOS.contains(new CountryDTO("Japan", 81, "Naruhito", "Shigeru Ishiba"));
+        System.out.println("present:"+present);
+        
     }
 }
